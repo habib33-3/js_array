7c27a8bcaf1b1d9011b51741df9018d8bf938d19
Habibur Rahman <habib.swi16@gmail.com>
2023-07-30 22:57:18 +0600

    3

---
+++ problem3.js
@@ -0,0 +1,19 @@
+let number1 = 13;
+let number2 = 79;
+let number3 = 45;
+
+if (number1 > number2) {
+  if (number1 > number3) {
+    console.log("number1 greatest");
+  } else {
+    console.log("number3 greatest");
+  }
+} else if (number2 > number1) {
+  if (number2 > number3) {
+    console.log("number2 gratest");
+  } else {
+    console.log("number3 greatest");
+  }
+} else {
+  console.log("number3 greatest");
+}
